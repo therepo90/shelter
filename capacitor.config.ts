5873aede078701714c408f5cf1fc574f28647eb3
Tomasz Szepczynski <tomasz.szepczynski@adtonos.com>
2025-12-25 19:13:32 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'shelter',
+  webDir: 'www'
+};
+
+export default config;
