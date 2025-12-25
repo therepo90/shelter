8041db84e66dbd0a9cc200945713f3d19e9de6df
Tomasz Szepczynski <tomasz.szepczynski@adtonos.com>
2025-12-25 19:23:06 +0100

    x

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// CHANGE: Call the element loader before the `bootstrapModule` call
+defineCustomElements(window);
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
