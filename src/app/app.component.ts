70b6391c2c9ea6b1eebf8d5028dca4ac872c61fd
Tomasz Szepczynski <tomasz.szepczynski@adtonos.com>
2025-12-28 14:59:59 +0100

    update git version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { SplashScreen } from '@capacitor/splash-screen'
 import { CapacitorUpdater } from '@capgo/capacitor-updater';
 import { Platform } from '@ionic/angular';
 import { DogsService } from "./tab1/dogs.service";
@@ -25,21 +26,29 @@ export class AppComponent {
 
   async checkForUpdate() {
     try {
+      console.log('Checking for updates...');
       const {version} = await firstValueFrom(this.dogsService.getCfg());
       const localVersion = await this.dogsService.getVersion();
       if (version && localVersion && version !== localVersion) {
+        console.log(`Updating from version ${localVersion} to ${version}`);
         const bundleInfo = await CapacitorUpdater.download({ url: 'https://therepo90.github.io/shelter/www.zip', version });
+        SplashScreen.show();
         await CapacitorUpdater.set({ id: bundleInfo.id });
         await this.dogsService.setVersion(version);
         window.location.reload();
       } else if (version && !localVersion) {
         // First run or no version stored, just save the version
+        console.log(`Setting initial version to ${version}`);
         await this.dogsService.setVersion(version);
+      }else{
+        console.log(`App is up to date with version ${localVersion}`);
       }
       this.initialized = true;
     } catch (e) {
       // ignore update errors
       alert('Update check error: ' + e);
+    }finally{
+      SplashScreen.hide()
     }
   }
 }
